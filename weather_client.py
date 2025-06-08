2d89bb42bba6be883be0e5436d3ee555b32b97bf
OceanicFjord <fynn.wachholz@rwth-aachen.de>
2025-06-08 18:09:06 +0200

    minor changes to weather_client.py

--- weather_client.py
+++ weather_client.py
@@ -11,3 +11,27 @@ class WeatherClient:
         if not api_key:
             raise ValueError("Ein gültiger API-Key wird benötigt.")
         self.api_key = api_key
+
+    def get_current(self, city: str) -> Dict:
+        URL = self.BASE_URL + "weather"
+        
+        params = {
+            "q": city,
+            "appid": self.api_key,
+            "units": "metric",
+            "lang": "de"
+        }
+
+        response = requests.get(url, params=params, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+
+        result = {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "humidity": data["main"]["humidity"],
+            "wind_speed": data["wind"]["speed"],
+            "description": data["weather"][0]["description"]
+        }
+        return result
+
